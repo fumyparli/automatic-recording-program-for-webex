de5710e006cab02a1f519a60f58cc0ff4b872bb1
fumyparli <qkskskn@naver.com>
2021-03-02 17:38:35 +0900

    Complete participation step

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ const myEmail = "qkskskm7@g.hongik.ac.kr";
 (async () => {
     const browser = await puppeteer.launch({
         headless: false,
-        devtools: true,
+        devtools: false,
     });
     const page = await browser.newPage();
     await page.goto(webUrl);
     await page.waitForSelector("#pbui_iframe");
     const frame = await page
         .frames()
-        .find((frame) => frame.name() === "pbui_iframe")
-        .waitForSelector(".style-box-2gTpv");
+        .find((frame) => frame.name() === "pbui_iframe");
+    await frame.waitForSelector(".style-box-2gTpv");
     await page.keyboard.press(" ");
     await page.keyboard.press("Tab");
     for (let i = 0; i < myEmail.length; i++) {
@@ -32,4 +32,22 @@ const myEmail = "qkskskm7@g.hongik.ac.kr";
         let btn = newHtml.childNodes[3].childNodes[0];
         btn.click();
     }, myName);
+    await frame.waitForSelector(
+        ".style-rest-1IrDU.style-theme-dark-iYE87.style-fte-btn-346LP.style-size-default-1y4cK.style-botton-outline-none-1M0ur"
+    );
+    await page.evaluate(() => {
+        let iframe = document.querySelector("#pbui_iframe");
+        let btn = iframe.contentWindow.document.querySelector(
+            ".style-rest-1IrDU.style-theme-dark-iYE87.style-fte-btn-346LP.style-size-default-1y4cK.style-botton-outline-none-1M0ur"
+        );
+        btn.click();
+    });
+    await frame.waitForSelector("#interstitial_join_btn");
+    await page.evaluate(() => {
+        let iframe = document.querySelector("#pbui_iframe");
+        let btn = iframe.contentWindow.document.querySelector(
+            "#interstitial_join_btn"
+        );
+        btn.click();
+    });
 })();
